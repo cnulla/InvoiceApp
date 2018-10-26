4000075f2d0fe8e59b14d964dffd0fbfb042c312
Camille <camillenulla28@gmail.com>
2018-10-26 14:52:18 +0800

    add path

--- invoiceapp/accounts/urls.py
+++ invoiceapp/accounts/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from accounts.views import (
     SignUpView,
     VerifyEmailView,
+    VerifyTokenView,
 )
 
 urlpatterns = [
         path('signup/', SignUpView.as_view(), name='signup'),
         path('verify-email', VerifyEmailView.as_view(), name='verify-email'),
+        path('verify-token/<str:token>', VerifyTokenView.as_view(), name='verify-token'),
 
 ]
