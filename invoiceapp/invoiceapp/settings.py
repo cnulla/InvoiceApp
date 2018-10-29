19f4831295380ed850669913a3e4f08f8f4e439c
Camille <camillenulla28@gmail.com>
2018-10-29 15:26:42 +0800

    updated

--- invoiceapp/invoiceapp/settings.py
+++ invoiceapp/invoiceapp/settings.py
@@ -124,9 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "assets"),
+    os.path.join(BASE_DIR, "assets/"),
 ]
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 try:
     from .local_settings import *
