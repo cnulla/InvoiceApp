53afc77ea076e5c30e4b1f7131f84d7694e25c8f
Camille <camillenulla28@gmail.com>
2018-10-30 11:32:40 +0800

    signin url

--- invoiceapp/accounts/urls.py
+++ invoiceapp/accounts/urls.py
@@ -3,10 +3,12 @@ from accounts.views import (
     SignUpView,
     VerifyEmailView,
     VerifyTokenView,
+    SignInView,
 )
 
 urlpatterns = [
         path('signup/', SignUpView.as_view(), name='signup'),
+        path('signin/', SignInView.as_view(), name='signin'),
         path('verify-email', VerifyEmailView.as_view(), name='verify-email'),
         path('verify-token/<str:token>/', VerifyTokenView.as_view(), name='verify-token'),
 
