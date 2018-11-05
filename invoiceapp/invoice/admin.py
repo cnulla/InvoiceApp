1b50a192e2e729873cb54e38c42ba2a0f1161a3e
Camille <camillenulla28@gmail.com>
2018-11-05 14:58:47 +0800

    client admin register

--- invoiceapp/invoice/admin.py
+++ invoiceapp/invoice/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from invoice.models import Client, Company, Invitation
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Company)
+admin.site.register(Invitation)
+
