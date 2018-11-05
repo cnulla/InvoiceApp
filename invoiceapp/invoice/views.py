8ccc640690fb7f3fc6609a687bfbe52ae367f9f7
Camille <camillenulla28@gmail.com>
2018-11-05 18:00:41 +0800

    update views

--- invoiceapp/invoice/views.py
+++ invoiceapp/invoice/views.py
@@ -2,8 +2,38 @@ from django.shortcuts import render, reverse, redirect
 from django.views.generic.base import TemplateView, View
 from django.http import HttpResponseRedirect
 
+from invoice.forms import ClientForm, CompanyForm
+
 # Create your views here.
 
 class DashboardView(TemplateView):
+    """ User Dashboard
+    """
     template_name = 'invoiceapp/dashboard.html'
+
+
+class ClientView(TemplateView):
+    """ List of Clients
+    """
+    template_name = 'invoiceapp/clients.html'
+
+
+class CreateClientView(TemplateView):
+    """ Create a client
+    """
+    template_name = 'invoiceapp/create_clients.html'
+
+
+    def get(self, *args, **kwargs):
+        form = ClientForm()
+        return render(self.request, self.template_name, {'form': form})
+
+
+    def post(self, *args, **kwargs):
+        form = ClientForm(self.request.POST)
+        if form.is_valid():
+            client = form.save(commit=False)
+            client.save()
+            return HttpResponseRedirect(reverse('client'))
+        return render(self.request, self.template_name, {'form': form})
 
