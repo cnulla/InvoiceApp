250ad8da99ae3a51f2180ddeac1597aa2f6f3cb9
Camille <camillenulla28@gmail.com>
2018-11-05 17:59:53 +0800

    add url

--- invoiceapp/invoice/urls.py
+++ invoiceapp/invoice/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from invoice.views import DashboardView
+from invoice.views import (
+    DashboardView,
+    CreateClientView,
+    ClientView,
+    )
 
 urlpatterns = [
-    path('', DashboardView.as_view(), name='dashboard')
+    path('', DashboardView.as_view(), name='dashboard'),
+    path('client/', ClientView.as_view(), name='client'),
+    path('create-client/', CreateClientView.as_view(), name='create-client'),
 
 ]
