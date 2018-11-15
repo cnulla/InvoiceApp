54cc20fcf0b30737f136a483502b7e714be917e7
Camille <camillenulla28@gmail.com>
2018-11-15 15:50:43 +0800

    add comments

--- invoiceapp/invoice/views.py
+++ invoiceapp/invoice/views.py
@@ -81,7 +81,7 @@ class CreateInvoiceView(InvoiceMixins,TemplateView):
 
     def post(self, *args, **kwargs):
         form = InvoiceForm(self.request.POST)
-        import pdb; pdb.set_trace();
+        import pdb; pdb.set_trace()
         if form.is_valid():
             invoice = form.save()
             items = self.request.POST.get('items')
