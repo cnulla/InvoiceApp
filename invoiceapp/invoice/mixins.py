47bebc495c4d283e4315607428b2105f82cc3269
Camille <camillenulla28@gmail.com>
2018-11-22 21:23:31 +0800

    fixed indentions, add doc string

--- invoiceapp/invoice/mixins.py
+++ invoiceapp/invoice/mixins.py
@@ -1,19 +1,21 @@
 from invoice.models import Item
 
 class InvoiceMixins(object):
-    """ Add item/order
-    """
+
     def add_item(self, invoice, item):
+         """ Add item/order
+        """
         item = Item(
             invoice=invoice, order_number=item.get('order_number'), order_description=item.get('order_description'),
             order_date=item.get('order_date'), end_date=item.get('end_date'), rate=item.get('rate'), total_hours=item.get('total_hours'),
             amount=item.get('amount'), total_amount=item.get('total_amount'), remarks=item.get('remarks'), item_type=item.get('item_type'),
             )
         item.save()
+
     def update_item(self, invoice, data):
-        import pdb; pdb.set_trace()
+        """Update Item
+        """
         item = Item.objects.get(id=data.get('id'))
-        # item.invoice = data.get('invoice')
         item.order_number = data.get('order_number')
         item.order_description = data.get('order_description')
         item.order_date = data.get('order_date')
